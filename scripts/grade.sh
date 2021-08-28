0149642b785c8d00ebe17fcef67e8b9ae1ef870e
Jiajie Chen <c@jia.je>
2021-08-28 21:32:08 +0800

    Add link to stackoverflow

--- scripts/grade.sh
+++ scripts/grade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# https://stackoverflow.com/questions/59895/how-can-i-get-the-source-directory-of-a-bash-script-from-within-the-script-itsel
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "${SCRIPT_DIR}/../"
 
